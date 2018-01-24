ba1bba698548e680a943eef7f2e33c4b927abb0f
markusbrandstaetter <32510118+markusbrandstaetter@users.noreply.github.com>
2018-01-24 08:45:55 +0100

    add frontend

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <div class="navbar  navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <router-link to="/" class="navbar-brand">HS-RO</router-link>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+            <li>
+              <router-link to="/" >Home</router-link>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><router-link to="/signup" class="nav-item nav-link"> Sign Up </router-link></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <img id="banner" src="../assets/banner.png" class="img-responsive" alt="Responsive image">
+    </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style type="text/css">
+  #banner {
+    margin-top: 70px;
+  }
+</style>
+
